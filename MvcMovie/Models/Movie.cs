6593c47ecad6b276137c76a543fec7d1f697c349
johnnybigoo <jotalico@icloud.com>
2022-03-23 23:25:04 -0400

    Add a new field

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,7 +7,7 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; }  
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
@@ -16,5 +16,6 @@ namespace MvcMovie.Models
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; }
     }
 }
