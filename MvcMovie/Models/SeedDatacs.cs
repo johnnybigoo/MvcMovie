3de3d040d9f493178a4df07ef94954aa71c98605
johnnybigoo <jotalico@icloud.com>
2022-03-24 14:00:08 -0400

    Rebuild after rebooting ~> caches

--- MvcMovie/Models/SeedDatacs.cs
+++ MvcMovie/Models/SeedDatacs.cs
@@ -35,6 +35,7 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
+                        Rating = "R",
                         Price = 8.99M
                     },
 
@@ -43,7 +44,8 @@ namespace MvcMovie.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Rating = "R",
+                        Price = 9.99M 
                     },
 
                     new Movie
@@ -51,6 +53,7 @@ namespace MvcMovie.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
+                        Rating = "R",
                         Price = 3.99M
                     }
                 );
